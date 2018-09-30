1ff1af72bc15975aae274590045a94a6ba525168
nogbit <marc@marcellusmiles.com>
2018-09-29 23:41:10 -0700

    correctly implemented for multiples

--- src/circularArc.js
+++ src/circularArc.js
@@ -3,51 +3,59 @@ import SVG from 'svg.js';
 
 export default () => {
   SVG.CircularArc = SVG.invent({
-    // fill = line or full, mode = half or full
-    create: function(config) {
-      // To Do...make sure min < max, initial is <= max and >= min
-      this.max = config.max;
-      this.min = config.min;
-      //this.setting = config.setting;
-      this.step = config.step;
-      this.radius = config.radius;
-      this.fill = config.fill;
-      this.mode = config.mode;
-      this.lineColor = config.lineColor;
-      this.lineWidth = config.lineWidth;
-      this.fullColor = config.fullColor;
-      this.setting(config.setting);
-  
-      this.startAngle = 0;
-      if (this.mode === 'half')
-        this.startAngle = -90.0;
-      if (this.fill === 'line') {
+
+    create: 'path',
+
+    inherit: SVG.Path,
+
+    extend: {
+      init: function(config) {
+        // To Do...make sure min < max, initial is <= max and >= min
+        this.setting(config.setting);
+        this.max(config.max);
+        this.min(config.min);
+        this.radius(config.radius);
+        this.lineWidth(config.lineWidth);
+        this.startAngle(-90.0);
+
+        // default colors
         this.attr('fill', '#000');
         this.attr('fill-opacity', 0);
-        this.attr('stroke', this.lineColor);
-        this.attr('stroke-width', this.lineWidth);
-      }
-    },
-    
-    inherit: SVG.Path,
-  
-    extend: {
-      setting: function(value) {
-        return this.attr('setting', value)
+        this.attr('stroke', config.lineColor);
+        this.attr('stroke-width', config.lineWidth);
+        return this;
+      },
+      max: function(val) {
+        return this.attr('max', val)
+      },
+      min: function(val) {
+        return this.attr('min', val)
+      },
+      radius: function(val) {
+        return this.attr('radius', val)
+      },
+      lineWidth: function(val) {
+        return this.attr('lineWidth', val)
+      },
+      startAngle: function(val) {
+        return this.attr('startAngle', val)
+      },
+      setting: function(val) {
+        return this.attr('setting', val)
       },
 
       plot: function (d) {
         if (d == null) {
-          var x     = this.attr('x') + this.radius + this.lineWidth;
-          var y     = this.attr('y') + this.radius + this.lineWidth;
-          var start = util.polarToCartesian(x, y, this.radius, this.angle());
-          var end   = util.polarToCartesian(x, y, this.radius, this.startAngle);
+          var x     = this.attr('x') + this.attr('radius') + this.attr('lineWidth');
+          var y     = this.attr('y') + this.attr('radius') + this.attr('lineWidth');
+          var start = util.polarToCartesian(x, y, this.attr('radius'), this.angle());
+          var end   = util.polarToCartesian(x, y, this.attr('radius'), this.attr('startAngle'));
   
-          var largeArcFlag = this.angle() - this.startAngle <= 180 ? "0" : "1";
+          var largeArcFlag = this.angle() - this.attr('startAngle') <= 180 ? "0" : "1";
   
           d = [
               "M", start.x, start.y, 
-              "A", this.radius, this.radius, 0, largeArcFlag, 0, end.x, end.y
+              "A", this.attr('radius'), this.attr('radius'), 0, largeArcFlag, 0, end.x, end.y
           ].join(" ");
         }
   
@@ -55,18 +63,17 @@ export default () => {
       },
   
       angle: function () {
-        var max = this.max;
-        var min = this.min;
-        var normal = (this.attr('setting') - min) / (max - min);
         //var denomral = (normal * (max - min) + min);
+        var normal = (this.attr('setting') - this.attr('min')) / (this.attr('max') - this.attr('min'));
         return (180 * normal) - 90
       }
     },
-    
+  
     construct: {
-      CircularArc: function(config) {
+      circulararc: function(config) {
         // TO DO...pass onValChange event to the constructor as well
-        return this.put(new SVG.CircularArc(config)).plot();
+        //return this.put(new SVG.CircularArc(config)).plot();
+        return this.put(new SVG.CircularArc).init(config).plot();
       }
     }
   })
